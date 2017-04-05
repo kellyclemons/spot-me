700600ce60dc7c218769cdfcb59f43ba794f75c7
Andrew Harris <blakebharris@gmail.com>
2017-04-05 13:42:31 -0500

    allow crossorigin

--- src/main/java/com/spot/me/controllers/SpotmeController.java
+++ src/main/java/com/spot/me/controllers/SpotmeController.java
@@ -5,12 +5,14 @@ import com.spot.me.services.UserRepository;
 import com.spot.me.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
+@CrossOrigin
 @RestController
 public class SpotmeController {
     @Autowired
